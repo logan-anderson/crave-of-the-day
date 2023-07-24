51de13acb1d6e094adf4449aa9c052ed8f0c3f94
Logan Anderson <landerson2@upei.ca>
2023-07-24 10:37:11 -0400

    add logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { Logo } from "@/components/logo";
+
 export const RainbowText = ({ children }: any) => {
   return (
     <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500">
@@ -13,6 +15,7 @@ export default function Home() {
       <div className="w-full container mx-auto">
         <div className="w-full flex items-center justify-between">
           <span className="text-yellow-300 no-underline hover:no-underline font-bold text-2xl lg:text-4xl">
+            <Logo />
             Snack{" "}
             <span className="bg-clip-text text-transparent bg-gradient-to-r from-pink-300 via-purple-300 to-teal-300">
               Of The Day
